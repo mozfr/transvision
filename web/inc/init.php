23633f27d4772be4a23c6224e47b4919c6530005
pascalc <pascal.chevrel@free.fr>
2012-09-13 14:48:11 +0200

    add basis for a front-controller

--- web/inc/init.php
+++ web/inc/init.php
@@ -1,8 +1,26 @@
 <?php
 
-require_once 'functions.php';
+/* routing */
 
-if (!valid($valid)) return;
+// We separate path and query string for the dispatcher
+$path  = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$query = parse_url($_SERVER['REQUEST_URI'], PHP_URL_QUERY);
+
+// Don't allow direct access to the file
+if ($path == '/inc/init.php') {
+    die('No direct access');
+}
+
+// Don't process non-PHP files
+$file_ext = pathinfo($path);
+if (isset($file_ext['extension']) && $file_ext['extension'] != 'php') {
+    return false;
+}
+
+/* end routing */
+
+$valid = true;
+require_once 'functions.php';
 
 // perf metrics
 $time_start = getmicrotime();
@@ -75,3 +93,66 @@ require_once INC . '/search_options.php';
 
 // include the cache files.
 require_once INC . 'cache_import.php';
+
+// Start output buffering, we will output in a template
+ob_start();
+
+if(valid($web_service)) {
+    // fonction de recherche
+    require_once INC . 'recherche.php';
+
+    foreach ($keys as $key => $chaine) {
+        $ken[$key][$chaine] = htmlspecialchars_decode($tmx_target[$key], ENT_QUOTES);
+    }
+    foreach ($keys2 as $key => $chaine) {
+        $kfr[$key][$chaine] = htmlspecialchars_decode($tmx_source[$key], ENT_QUOTES);
+    }
+
+    $json_en = json_encode($ken);
+    $json_fr = json_encode($kfr);
+
+
+    header('Content-type: application/json; charset=UTF-8');
+
+    if (isset($_GET['callback'])) {
+        if ($_GET['return_loc'] == 'loc') {
+            echo $_GET['callback'] . '(' . $json_fr . ');';
+        } else{
+            echo $_GET['callback'] . '(' . $json_en . ');';
+        }
+    } else {
+        if (isset($_GET['return_loc']) && $_GET['return_loc'] == 'loc'){
+            echo $json_fr;
+        } else {
+            echo htmlspecialchars_decode($json_en, ENT_QUOTES);
+        }
+    }
+    // end of webservice code
+    // XXX: factorize more code with normal display
+    exit;
+}
+
+// Base html
+require_once VIEWS . 'search_form.php';
+
+// fonction de recherche
+if ($check['t2t']) {
+    require_once VIEWS . 't2t.php';
+} else {
+    require_once INC . 'recherche.php';
+
+    // result presentation
+    if($recherche != '') {
+        if ($check['ent']) {
+            require_once VIEWS . 'results_ent.php';
+        } else {
+            require_once VIEWS . 'results.php';
+        }
+    }
+}
+
+$content = ob_get_contents();
+ob_end_clean();
+
+// display the page
+require_once VIEWS .'template.php';
