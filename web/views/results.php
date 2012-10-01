4b7027c06a524dacc650856c491a5cc44a4aa771
pascalc <pascal.chevrel@free.fr>
2012-10-01 18:30:33 +0200

    fix bug filtering duplicate strings

--- web/views/results.php
+++ web/views/results.php
@@ -5,11 +5,13 @@ if (!valid($valid)) return;
 // The search results are displayed into a table
 // (initial_search is the original sanitized searched string before any modification)
 
-$search_results = results(array_flip($keys), $tmx_source, $tmx_target);
+$search_results = results(array_keys($keys), $tmx_source, $tmx_target);
+
 echo '  <h2><span class="searchedTerm">' . $initial_search . '</span> is in ' . $sourceLocale . ' in:</h2>';
 echo resultsTable($search_results, $recherche, $sourceLocale, $locale, false, $check);
 
-$search_results = results(array_flip($keys2), $tmx_source, $tmx_target);
+$search_results = results(array_keys($keys2), $tmx_source, $tmx_target);
+
 echo '  <h2><span class="searchedTerm">' . $initial_search . '</span> is in ' . $locale . ' in:</h2>';
 echo resultsTable($search_results, $recherche, $sourceLocale, $locale, true, $check);
 
