0fe7391ba4dde8d8c1dd7534e6872f8158d1f0c3
pascalc <pascal.chevrel@free.fr>
2012-10-02 18:31:34 +0200

    remove mxr link for gaia apps on entty search

--- web/views/results_ent.php
+++ web/views/results_ent.php
@@ -14,13 +14,21 @@ $table .= "      <th>Entity</th>\n";
 $table .= "      <th>" . $sourceLocale . "</th>\n";
 $table .= "      <th>" . $locale . "</th>\n";
 $table .= "    </tr>\n\n";
-
+// mxr support
+$mxr_url  = "http://mxr.mozilla.org/comm-${check['repo']}/search?find=";
+$mxr_field_limit = 27;
 foreach ($entities as $val) {
     // let's analyse the entity for the search string
     $search = explode(':', $val);
-    $search = $search[0] . '.*' . $search[1] . '&amp;string=' . $search[2];
     $mxr_url  = "http://mxr.mozilla.org/comm-${check['repo']}/search?find=";
-    $mxr_link = '<a href="' . $mxr_url . $search . '">' . formatEntity($val) . '</a>';
+    
+    if($search[0] == 'apps') {
+        $mxr_link = formatEntity($val);
+    } else {            
+        // we chop search strings with mb_strimwidth() because  of field length limits in mxr)
+        $search = mb_strimwidth($search[0] . '.*' . $search[1], 0, $mxr_field_limit) . '&amp;string=' . mb_strimwidth($search[2], 0, 29);
+        $mxr_link = '<a href="' . $mxr_url . $search . '">' . formatEntity($val) . '</a>';
+    }
 
     $target_string = str_replace(' ', '<span class="highlight-gray"> </span>',  $tmx_target[$val]); // nbsp highlight
     $table .= "    <tr>\n";
