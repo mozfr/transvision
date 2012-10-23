a9d9364fb9ea55bafb4908e73d4a3bbfec394441
pascalc <pascal.chevrel@free.fr>
2012-10-23 11:02:58 +0200

    add CORS headers to the json API

--- web/views/webservice.php
+++ web/views/webservice.php
@@ -7,5 +7,6 @@ foreach ($locale1_strings as $key => $str) {
 }
 
 header('Content-type: application/json; charset=UTF-8');
+header("access-control-allow-origin: *");
 echo json_encode($json_search_results);
 exit;
