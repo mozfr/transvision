716d7b8a212b29bb88bd521b35225ac682e80ee8
pascalc <pascal.chevrel@free.fr>
2012-10-23 12:49:41 +0200

    add JSONP support to webservice

--- web/views/webservice.php
+++ web/views/webservice.php
@@ -1,12 +1,22 @@
 <?php
 
-$json_search_results = array();
+$json = array();
 
 foreach ($locale1_strings as $key => $str) {
-    $json_search_results[$key][$str] = htmlspecialchars_decode($tmx_target[$key], ENT_QUOTES);
+    $json[$key][$str] = htmlspecialchars_decode($tmx_target[$key], ENT_QUOTES);
 }
 
-header('Content-type: application/json; charset=UTF-8');
+$json = json_encode($json);
+
 header("access-control-allow-origin: *");
-echo json_encode($json_search_results);
-exit;
+
+if (!isset($_GET['callback'])) {
+    // JSON if no callback
+    header('Content-type: application/json; charset=UTF-8');
+} else {
+    // JSONP with the defined callback
+    header('Content-type: application/javascript; charset=UTF-8');
+    $json = $_GET['callback'] . '(' . $json . ')';
+}
+
+exit($json);
