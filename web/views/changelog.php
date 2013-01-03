98bb123cf90cc276d76ff1a81821a79106b420cb
pascalc <pascal.chevrel@free.fr>
2013-01-04 00:28:46 +0100

    forbid single letter searches because they return megabytes of badly formatted html, which hangs Firefox

--- web/views/changelog.php
+++ web/views/changelog.php
@@ -1,25 +1,17 @@
 <?php $title = '<a href="/">Transvision</a> changelog'; ?>
 
-<h2 id="v1.8">Version 1.8 - 2012-??-??</h2>
-<h3>End user visible changes</h3>
-<ul>
-    <li></li>
-</ul>
-<h3>Developer visible changes</h3>
-<ul>
-    <li></li>
-</ul>
-
 <h2 id="v1.8">Version 1.8 - 2013-??-??</h2>
+<p>This should be a transparent release for users, the 1.8 release is mostly about using PHP Composer dependency manager for external dependencies (locale detection, unit testing, logging). </p>
+
 <h3>End user visible changes</h3>
 <ul>
-    <li></li>
+    <li>Searches need to be at least 2 characters long, single letter searches now return an error message.</li>
 </ul>
-
 
 <h3>Other changes</h3>
 <ul>
-    <li>Added Monolog library to be able to log events (/ex: which locales actually use Transvision)</li>
+    <li>External dependencies are now all managed through Composer</li>
+    <li>Added Monolog library to be able to log events related to debugging or the use of the application (/ex: which locales actually use Transvision)</li>
 </ul>
 
 <h2 id="v1.7">Version 1.7 - 2012-10-24</h2>
--- web/views/results.php
+++ web/views/results.php
@@ -1,15 +1,20 @@
 <?php
 
+if (strlen(trim($recherche)) < 2) {
+    echo '<p><strong>Search term should be at least 2 characters long.</strong></p>';
+    return;
+}
+
 // The search results are displayed into a table
 // (initial_search is the original sanitized searched string before any modification)
 
 $results = new TransvisionResults\ShowResults();
 $search_results = $results->getTMXResults(array_keys($locale1_strings), $tmx_source, $tmx_target);
 
-echo '  <h2><span class="searchedTerm">' . $initial_search . '</span> is in ' . $sourceLocale . ' in:</h2>';
+echo '<h2><span class="searchedTerm">' . $initial_search . '</span> is in ' . $sourceLocale . ' in:</h2>';
 echo resultsTable($search_results, $recherche, $sourceLocale, $locale, false, $check);
 
 $search_results = results(array_keys($locale2_strings), $tmx_source, $tmx_target);
 
-echo '  <h2><span class="searchedTerm">' . $initial_search . '</span> is in ' . $locale . ' in:</h2>';
+echo '<h2><span class="searchedTerm">' . $initial_search . '</span> is in ' . $locale . ' in:</h2>';
 echo resultsTable($search_results, $recherche, $sourceLocale, $locale, true, $check);
