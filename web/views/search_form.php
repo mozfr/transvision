6e5bd45e50601b53c70cbb0ae594527014e736c6
pascalc <pascal.chevrel@free.fr>
2013-01-04 02:28:48 +0100

    more visual improvements

--- web/views/search_form.php
+++ web/views/search_form.php
@@ -54,7 +54,7 @@ switch($check['repo']) {
 
 ?>
 
-  <div id="current" onclick="javascript:t2t();">You are looking at the <?=$check['repo']?> channel (<?=$locale?>)</div>
+  <div id="current" onclick="javascript:t2t();">You are looking at the <?=$check['repo']?> channel <strong><?=$locale?></strong></div>
     <form name="searchform" method="get" action="./" >
         <fieldset id="main">
 
@@ -77,21 +77,21 @@ switch($check['repo']) {
                 <input type="radio" name="repo" value="aurora"  id="aurora"  <?=$au?> ><label for="aurora">Aurora</label>
                 <input type="radio" name="repo" value="beta"    id="beta"    <?=$be?> ><label for="beta">Beta</label>
                 <input type="radio" name="repo" value="release" id="release" <?=$re?> ><label for="release">Release</label>
-                <input type="radio" name="repo" value="gaia"    id="gaia"    <?=$ga?> ><label for="gaia">Gaia only</label>
+                <input type="radio" name="repo" value="gaia"    id="gaia"    <?=$ga?> ><label for="gaia">Gaia</label>
             </fieldset>
 
             <fieldset>
                 <legend>Search options</legend>
                 <input type="checkbox" name="case_sensitive" id="case_sensitive" value="case_sensitive" <?=checkboxState($check['case_sensitive'])?> />
-                <label for="case_sensitive">Case sensitive</label>
+                <label for="case_sensitive">Case Sensitive</label>
                 <input type="checkbox" name="wild" id="wild" value="wild"          <?=checkboxState($check['wild'])?> />
-                <label for="wild">* Wildcard</label>
+                <label for="wild">Wildcard (*)</label>
                 <input type="checkbox" name="whole_word" id="whole_word" value="whole_word" <?=checkboxState($check['whole_word'])?> />
                 <label for="whole_word">Whole Word</label>
                 <input type="checkbox" name="ent" id="ent" value="ent" <?=checkboxState($check['ent'])?> />
-                <label for="ent">Entity Search</label>
+                <label for="ent">Entity</label>
                 <input type="checkbox" name="perfect_match" id="perfect_match" value="perfect_match" <?=checkboxState($check['perfect_match'])?> />
-                <label for="perfect_match">Perfect match</label>
+                <label for="perfect_match">Perfect Match</label>
                 <input type="checkbox" name="t2t" id="t2t" value="t2t"  <?=checkboxState($check['t2t'])?> onclick="uncheck();"/>
                 <label for="t2t">Glossary</label>
             </fieldset>
