d658d1811e85b1b769bc551b8d76b337c3084e98
pascalc <pascal.chevrel@free.fr>
2013-01-08 17:02:14 +0100

    part of issue 18, preg escape searched terms to allow searching strings with [({ in them

--- web/inc/recherche.php
+++ web/inc/recherche.php
@@ -1,19 +1,17 @@
 <?php
 
-// If $recherche consits of several words, each are stocked on the $aaa variable
-$aaa = explode(' ', $recherche);
-
-// If case sensitive is checked,
-// The search is made for each word (aa) of the searched string (aaa)
-
-$b               = ($check['whole_word']) ? '\b' : '';
-$i               = ($check['case_sensitive'])  ? ''   : 'i';
-$search          = '/' . $b . $aaa[0] . $b . '/' . $i;
+// If $recherche consits of several words, each are stocked oin the $exploded_search array
+$exploded_search = explode(' ', $recherche);
+$whole_word      = ($check['whole_word']) ? '\b' : '';
+$case_sensitive  = ($check['case_sensitive']) ? '' : 'i';
+$search          = '/' . $whole_word . preg_quote($exploded_search[0]) . $whole_word . '/' . $case_sensitive;
 $locale1_strings = preg_grep($search, $tmx_source);
 $locale2_strings = preg_grep($search, $tmx_target);
 $entities        = preg_grep($search, array_keys($tmx_source));
 
-foreach ($aaa as $aa) {
-    $locale1_strings = preg_grep("/{$b}{$aa}{$b}/{$i}", $locale1_strings);
-    $locale2_strings = preg_grep("/{$b}{$aa}{$b}/{$i}", $locale2_strings);
+// The search is done for each word ($word) of the searched string ($exploded_search)
+foreach ($exploded_search as $word) {
+    $word = preg_quote($word);
+    $locale1_strings = preg_grep("/{$whole_word}{$word}{$whole_word}/{$case_sensitive}", $locale1_strings);
+    $locale2_strings = preg_grep("/{$whole_word}{$word}{$whole_word}/{$case_sensitive}", $locale2_strings);
 }
