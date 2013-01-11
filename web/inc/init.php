b1ddcb81bc202feea4632c5dc43273a0396aae2e
pascalc <pascal.chevrel@free.fr>
2013-01-11 17:44:40 +0100

    1.9

--- web/inc/init.php
+++ web/inc/init.php
@@ -14,7 +14,7 @@ define('INC', INSTALLROOT . 'web/inc/');
 define('VIEWS', INSTALLROOT . 'web/views/');
 
 // Bump this constant with each new release
-const VERSION = '1.9dev';
+const VERSION = '1.9';
 
 
 define('DEBUG', (strstr(VERSION, 'dev') || isset($_GET['debug'])) ? true : false);
