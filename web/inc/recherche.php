017c52f49e046a936b516d443e04b86fd0492c7a
pascalc <pascal.chevrel@free.fr>
2013-01-13 17:03:28 +0100

    escape regex delimiter character in searches, fixes issue #31

--- web/inc/recherche.php
+++ web/inc/recherche.php
@@ -4,14 +4,14 @@
 $exploded_search = explode(' ', $recherche);
 $whole_word      = ($check['whole_word']) ? '\b' : '';
 $case_sensitive  = ($check['case_sensitive']) ? '' : 'i';
-$search          = '/' . $whole_word . preg_quote($exploded_search[0]) . $whole_word . '/' . $case_sensitive;
+$search          = '/' . $whole_word . preg_quote($exploded_search[0], '/') . $whole_word . '/' . $case_sensitive;
 $locale1_strings = preg_grep($search, $tmx_source);
 $locale2_strings = preg_grep($search, $tmx_target);
 $entities        = preg_grep($search, array_keys($tmx_source));
 
 // The search is done for each word ($word) of the searched string ($exploded_search)
 foreach ($exploded_search as $word) {
-    $word = preg_quote($word);
+    $word = preg_quote($word, '/');
     $locale1_strings = preg_grep("/{$whole_word}{$word}{$whole_word}/{$case_sensitive}", $locale1_strings);
     $locale2_strings = preg_grep("/{$whole_word}{$word}{$whole_word}/{$case_sensitive}", $locale2_strings);
 }
