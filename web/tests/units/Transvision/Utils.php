ff8b5437cf13d0dc89875401a44ab1434b1c7554
pascalc <pascal.chevrel@free.fr>
2013-02-01 18:13:25 +0100

    more unit tests

--- web/tests/units/Transvision/Utils.php
+++ web/tests/units/Transvision/Utils.php
@@ -51,4 +51,85 @@ class Utils extends atoum\test
         ;
     }
 
+    public function dataProvider1_checkBoxState()
+    {
+        $_GET['t2t'] = "somedata";
+        return array(
+            array($_GET['t2t'], '')
+        );
+    }
+
+    /**
+     * @dataProvider dataProvider1_checkBoxState
+     */
+
+    public function testCheckboxState1($a, $b)
+    {
+        $obj = new \Transvision\Utils();
+        $this
+            ->string($obj->checkboxState($a, $b))
+                ->isEqualTo(' disabled="disabled"')
+        ;
+    }
+
+    public function dataProvider2_checkBoxState()
+    {
+        $_GET['t2t'] = "somedata";
+        return array(
+            array($_GET['t2t'], 't2t')
+        );
+    }
+
+    /**
+     * @dataProvider dataProvider2_checkBoxState
+     */
+
+    public function testCheckboxState2($a, $b)
+    {
+        $obj = new \Transvision\Utils();
+        $this
+            ->string($obj->checkboxState($a, $b))
+                ->isEqualTo(' checked="checked"')
+        ;
+    }
+
+    public function dataProvider3_checkBoxState()
+    {
+        return array(
+            array('some string', null)
+        );
+    }
+
+    /**
+     * @dataProvider dataProvider3_checkBoxState
+     */
+
+    public function testCheckboxState3($a, $b)
+    {
+        $obj = new \Transvision\Utils();
+        $this
+            ->string($obj->checkboxState($a, $b))
+                ->isEqualTo(' checked="checked"')
+        ;
+    }
+
+    public function dataProvider_formatEntity()
+    {
+        return array(
+            array('browser/chrome/browser/browser.dtd:historyHomeCmd.label', false)
+        );
+    }
+
+    /**
+     * @dataProvider dataProvider_formatEntity
+     */
+    public function testFormatEntity($a, $b)
+    {
+        $obj = new \Transvision\Utils();
+        $this
+            ->string($obj->formatEntity($a, $b))
+                ->isEqualTo('<span class="green">browser</span><span class="superset">&nbsp;&sup;&nbsp;</span>chrome<span class="superset">&nbsp;&sup;&nbsp;</span>browser<span class="superset">&nbsp;&sup;&nbsp;</span>browser.dtd<br><span class="red">historyHomeCmd.label</span>')
+        ;
+    }
+
 }
