8b082f156d667aebdc22d0b555a248665c7d23a6
pascalc <pascal.chevrel@free.fr>
2013-02-19 17:37:48 +0100

    whitespace nit

--- web/inc/variables.php
+++ web/inc/variables.php
@@ -1,6 +1,7 @@
 <?php
 
 // Global variable used across the project
-$repos = array('release', 'beta', 'aurora', 'central', 'gaia');
+$repos         = array('release', 'beta', 'aurora', 'central', 'gaia');
 $desktop_repos = array_diff($repos, array('gaia'));
-$spanishes = array('es-AR', 'es-CL', 'es-ES', 'es-MX');
+$spanishes     = array('es-AR', 'es-CL', 'es-ES', 'es-MX');
+$tmx           = array();
