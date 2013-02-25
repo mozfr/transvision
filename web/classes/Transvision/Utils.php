b7e4b74e6b4dfeda5b8c672abfcbf45960145079
pascalc <pascal.chevrel@free.fr>
2013-02-25 13:42:35 +0100

    create a jsonOutput() static method to output the webservice json data and make it testable

--- web/classes/Transvision/Utils.php
+++ web/classes/Transvision/Utils.php
@@ -457,4 +457,24 @@ class Utils
         return $file_list;
     }
 
+    /*
+     * Return a json/jsonp representation of data and exits;
+     *
+     * @param  array  data in json field
+     * @param  string jsonp function name, default to false
+     * @return json feed
+     */
+    public static function jsonOutput(array $data, $jsonp = false) {
+        $json = json_encode($data);
+        $mime = 'application/json';
+
+        if ($jsonp && is_string($jsonp)) {
+            $mime = 'application/javascript';
+            $json = $jsonp . '(' . $json . ')';
+        }
+
+        header("access-control-allow-origin: *");
+        header("Content-type: {$mime}; charset=UTF-8");
+        return $json;
+    }
 }
--- web/views/webservice.php
+++ web/views/webservice.php
@@ -3,21 +3,13 @@
  * This view outputs a json or jsonp representation of search results
  */
 
+namespace Transvision;
+
 $json = array();
+$callback  = isset($_GET['callback']) ? $_GET['callback'] : false;
 
 foreach ($locale1_strings as $key => $str) {
     $json[$key][$str] = htmlspecialchars_decode($tmx_target[$key], ENT_QUOTES);
 }
 
-$json = json_encode($json);
-$mime = 'application/json';
-
-if (isset($_GET['callback'])) {
-    // JSONP with the defined callback
-    $mime = 'application/javascript';
-    $json = $_GET['callback'] . '(' . $json . ')';
-}
-
-header("access-control-allow-origin: *");
-header("Content-type: {$mime}; charset=UTF-8");
-exit($json);
+echo Utils::jsonOutput($json, $callback);
