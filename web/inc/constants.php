c0939f47499ae6757590d9a28bebe09e357593cc
pascalc <pascal.chevrel@free.fr>
2013-03-23 16:37:03 -0700

    Put constant for version number at top

--- web/inc/constants.php
+++ web/inc/constants.php
@@ -1,5 +1,8 @@
 <?php
 
+// Bump this constant with each new release
+const VERSION = '2.4dev';
+
 // Constants for the project
 define('DATAROOT', $ini_array['root']);
 define('HG', $ini_array['local_hg'] . '/');
@@ -8,9 +11,6 @@ define('INSTALLROOT', $ini_array['install'] . '/');
 define('WEBROOT', INSTALLROOT . 'web/');
 define('INC', INSTALLROOT . 'web/inc/');
 define('VIEWS', INSTALLROOT . 'web/views/');
-
-// Bump this constant with each new release
-const VERSION = '2.4dev';
 
 // Special modes for the app
 define('DEBUG', (strstr(VERSION, 'dev') || isset($_GET['debug'])) ? true : false);
