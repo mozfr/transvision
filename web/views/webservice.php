fd72283ff66cafc6a321cad4e8cb54694e8ccc39
pascalc <pascal.chevrel@free.fr>
2013-04-17 16:08:29 +0200

    Issue #66 : provide a reliable web service for serches on entities

--- web/views/webservice.php
+++ web/views/webservice.php
@@ -6,10 +6,18 @@
 namespace Transvision;
 
 $json = array();
-$callback  = isset($_GET['callback']) ? $_GET['callback'] : false;
+$callback = isset($_GET['callback']) ? $_GET['callback'] : false;
 
-foreach ($locale1_strings as $key => $str) {
-    $json[$key][$str] = htmlspecialchars_decode($tmx_target[$key], ENT_QUOTES);
+if ($check['search_type'] == 'strings') {
+    foreach ($locale1_strings as $key => $str) {
+        $json[$key][$str] = htmlspecialchars_decode($tmx_target[$key], ENT_QUOTES);
+    }
+}
+
+if ($check['search_type'] == 'entities') {
+    foreach ($entities as $key) {
+        $json[$key][$tmx_source[$key]] = htmlspecialchars_decode($tmx_target[$key], ENT_QUOTES);
+    }
 }
 
 echo Utils::jsonOutput($json, $callback);
