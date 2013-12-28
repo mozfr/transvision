acbfe9b1a7f3fb6bf360559e9932ce22857c51fb
pascalc <pascal.chevrel@free.fr>
2013-12-28 20:26:48 +0100

    fix tab vs spaces indentation in a file

--- web/inc/onestring_model.php
+++ web/inc/onestring_model.php
@@ -2,31 +2,31 @@
 namespace Transvision;
 
 $repo = isset($_GET['repo']) && in_array($_GET['repo'], $repos)
-		? $_GET['repo']
-		: 'release';
+        ? $_GET['repo']
+        : 'release';
 
 $strings = Utils::getRepoStrings('en-US', $repo);
 $entity = isset($_GET['entity']) ? $_GET['entity'] : false;
 
 // Invalid entity, we don't do any calculation and get back to the view
 if (!$entity) {
-	$error = 1;
-	return;
+    $error = 1;
+    return;
 } elseif (!array_key_exists($entity, $strings)) {
-	$error = 2;
-	return;
+    $error = 2;
+    return;
 }
 
 $translations = array('en-US' => $strings[$entity]);
 $locales = Utils::getFilenamesInFolder(TMX . $repo . '/');
 
 foreach($locales as $locale) {
-	$strings = Utils::getRepoStrings($locale, $repo);
-	if (array_key_exists($entity, $strings)) {
-		$translations[$locale] = $strings[$entity];
-	} else {
-		$translations[$locale] = false;
-	}
+    $strings = Utils::getRepoStrings($locale, $repo);
+    if (array_key_exists($entity, $strings)) {
+        $translations[$locale] = $strings[$entity];
+    } else {
+        $translations[$locale] = false;
+    }
 }
 unset($strings);
 
