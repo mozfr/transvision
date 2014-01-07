3efc19818401d325a5aed99bfb0cf35f14692069
Francesco Lodolo (:flod) <flod@lodolo.net>
2014-01-07 13:44:02 +0100

    Enable responsive view default on small screens, fix more CSS

--- web/views/search_form.php
+++ web/views/search_form.php
@@ -53,147 +53,151 @@ $cookie_target_locale2 = $get_cookie('default_target_locale2');
 ?>
 
   <div id="current" onclick="javascript:t2t();">You are looking at the <em><?=$repos_nice_names[$check['repo']]?></em> channel <strong><?=$locale?></strong></div>
-    <form name="searchform" method="get" action="./" >
+    <form name="searchform" id="searchform" method="get" action="./" >
         <fieldset id="main">
             <div id="search">
+                <p class="smallscreen_notices">Use the <a href="" class="menu-button">Menu tab</a> in the right top corner to select a different view.<p>
                 <input type="text" name="recherche" id="recherche" value="<?=$initial_search?>" placeholder="Type your search here" size="30" />
                 <input type="submit" value="Search" alt="Search" />
                 <p id="searchcontext">Search will be performed on: <span id="searchcontextvalue"><?=$search_type_descriptions[$check['search_type']]?></span>.</p>
             </div>
-            <fieldset>
-                <legend>Repository</legend>
-                <select id='repository' name='repo'  onchange="changeSourceTargetValues(this.value);">
-                <?=$repo_list?>
-                </select>
-                <label class="default_option">
-                    <input type="checkbox"
-                           id="default_repository"
-                           value="<?=$check['repo']?>"
-                           data-cookie="<?=$cookie_repository?>"
-                           onclick="setCookie('default_repository',this.value,3650);"
-                           <?=Utils::checkboxDefaultOption($check['repo'], $cookie_repository)?>
-                     /> <span>Default</span>
-                 </label>
-            </fieldset>
-            <fieldset>
-                <legend>Source Locale</legend>
-                <select id='source_locale' name='sourcelocale' onchange="changeDefaultSource('source_locale');">
-                <?=$source_locales_list[$check['repo']]?>
-                </select>
-                <label class="default_option">
-                    <input type="checkbox"
-                           id="default_source_locale"
-                           value="<?=$source_locale?>"
-                           data-cookie="<?=$cookie_source_locale?>"
-                           onclick="setCookie('default_source_locale',this.value,3650);"
-                            <?php
-                            // Mark as default only if the cookie_source_locale exist in repository array
-                            if (in_array($cookie_source_locale, $loc_list[$check['repo']])) {
-                                echo Utils::checkboxDefaultOption($source_locale, $cookie_source_locale);
-                            }
-                            ?>
-                     /> <span>Default</span>
-                 </label>
-            </fieldset>
-            <fieldset>
-                <legend>Target Locale</legend>
-                <select id='target_locale' name='locale' onchange="changeDefaultSource('target_locale');">
-                <?=$target_locales_list[$check['repo']]?>
-                </select>
-                <label class="default_option">
-                    <input type="checkbox"
-                           id="default_target_locale"
-                           value="<?=$locale?>"
-                           data-cookie="<?=$cookie_target_locale?>"
-                           onclick="setCookie('default_target_locale',this.value,3650);"
-                            <?php
-                            // Mark as default only if the cookie_target_locale exist in repository array
-                            if (in_array($cookie_target_locale, $loc_list[$check['repo']])) {
-                                echo Utils::checkboxDefaultOption($locale, $cookie_target_locale);
-                            }
-                            ?>
-                     /> <span>Default</span>
-                 </label>
-            </fieldset>
+            <div id="searchoptions">
+                <fieldset>
+                    <legend>Repository</legend>
+                    <select id='repository' name='repo'  onchange="changeSourceTargetValues(this.value);">
+                    <?=$repo_list?>
+                    </select>
+                    <label class="default_option">
+                        <input type="checkbox"
+                               id="default_repository"
+                               value="<?=$check['repo']?>"
+                               data-cookie="<?=$cookie_repository?>"
+                               onclick="setCookie('default_repository',this.value,3650);"
+                               <?=Utils::checkboxDefaultOption($check['repo'], $cookie_repository)?>
+                         /> <span>Default</span>
+                     </label>
+                </fieldset>
+                <fieldset>
+                    <legend>Source Locale</legend>
+                    <select id='source_locale' name='sourcelocale' onchange="changeDefaultSource('source_locale');">
+                    <?=$source_locales_list[$check['repo']]?>
+                    </select>
+                    <label class="default_option">
+                        <input type="checkbox"
+                               id="default_source_locale"
+                               value="<?=$source_locale?>"
+                               data-cookie="<?=$cookie_source_locale?>"
+                               onclick="setCookie('default_source_locale',this.value,3650);"
+                                <?php
+                                // Mark as default only if the cookie_source_locale exist in repository array
+                                if (in_array($cookie_source_locale, $loc_list[$check['repo']])) {
+                                    echo Utils::checkboxDefaultOption($source_locale, $cookie_source_locale);
+                                }
+                                ?>
+                         /> <span>Default</span>
+                     </label>
+                </fieldset>
+                <fieldset>
+                    <legend>Target Locale</legend>
+                    <select id='target_locale' name='locale' onchange="changeDefaultSource('target_locale');">
+                    <?=$target_locales_list[$check['repo']]?>
+                    </select>
+                    <label class="default_option">
+                        <input type="checkbox"
+                               id="default_target_locale"
+                               value="<?=$locale?>"
+                               data-cookie="<?=$cookie_target_locale?>"
+                               onclick="setCookie('default_target_locale',this.value,3650);"
+                                <?php
+                                // Mark as default only if the cookie_target_locale exist in repository array
+                                if (in_array($cookie_target_locale, $loc_list[$check['repo']])) {
+                                    echo Utils::checkboxDefaultOption($locale, $cookie_target_locale);
+                                }
+                                ?>
+                         /> <span>Default</span>
+                     </label>
+                </fieldset>
 
-<?php if ($url['path'] == '3locales'): ?>
-            <fieldset>
-                <legend>Extra Locale</legend>
-                <select id='target_locale2' name='locale2' onchange="changeDefaultSource('target_locale2');">
-                <?=$target_locales_list2[$check['repo']]?>
-                </select>
-                <label class="default_option">
-                    <input type="checkbox"
-                           id="default_target_locale2"
-                           value="<?=$locale2?>"
-                           data-cookie="<?=$cookie_target_locale2?>"
-                           onclick="setCookie('default_target_locale2',this.value,3650);"
-                            <?php
-                            // Mark as default only if the cookie_target_locale exist in repository array
-                            if (in_array($cookie_target_locale2, $loc_list[$check['repo']])) {
-                                echo Utils::checkboxDefaultOption($locale2, $cookie_target_locale2);
-                            }
-                            ?>
-                     /> <span>Default</span>
-                 </label>
-            </fieldset>
-<?php endif; ?>
+    <?php if ($url['path'] == '3locales'): ?>
+                <fieldset>
+                    <legend>Extra Locale</legend>
+                    <select id='target_locale2' name='locale2' onchange="changeDefaultSource('target_locale2');">
+                    <?=$target_locales_list2[$check['repo']]?>
+                    </select>
+                    <label class="default_option">
+                        <input type="checkbox"
+                               id="default_target_locale2"
+                               value="<?=$locale2?>"
+                               data-cookie="<?=$cookie_target_locale2?>"
+                               onclick="setCookie('default_target_locale2',this.value,3650);"
+                                <?php
+                                // Mark as default only if the cookie_target_locale exist in repository array
+                                if (in_array($cookie_target_locale2, $loc_list[$check['repo']])) {
+                                    echo Utils::checkboxDefaultOption($locale2, $cookie_target_locale2);
+                                }
+                                ?>
+                         /> <span>Default</span>
+                     </label>
+                </fieldset>
+    <?php endif; ?>
 
-            <fieldset>
-                <legend>Search in</legend>
-                <select name='search_type' id='search_type' onchange="changeSearchContext(this);">
-                <?=$search_type_list?>
-                </select>
-            </fieldset>
+                <fieldset>
+                    <legend>Search in</legend>
+                    <select name='search_type' id='search_type' onchange="changeSearchContext(this);">
+                    <?=$search_type_list?>
+                    </select>
+                </fieldset>
 
-            <fieldset id="fs_advanced">
-                <legend>Advanced Search options</legend>
-                <span>
-                    <input type="checkbox"
-                           name="case_sensitive"
-                           id="case_sensitive"
-                           value="case_sensitive"
-                           <?=Utils::checkboxState($check['case_sensitive'])?>
-                     />
-                    <label for="case_sensitive">Case Sensitive</label>
-                </span>
-                <span>
-                    <input type="checkbox"
-                           name="wild"
-                           id="wild"
-                           value="wild"
-                           <?=Utils::checkboxState($check['wild'])?>
-                     />
-                    <label for="wild">Wildcard (*)</label>
-                </span>
-                <span>
-                    <input type="checkbox"
-                           name="whole_word"
-                           id="whole_word"
-                           value="whole_word"
-                           <?=Utils::checkboxState($check['whole_word'])?>
+                <fieldset id="fs_advanced">
+                    <legend>Advanced Search options</legend>
+                    <span>
+                        <input type="checkbox"
+                               name="case_sensitive"
+                               id="case_sensitive"
+                               value="case_sensitive"
+                               <?=Utils::checkboxState($check['case_sensitive'])?>
+                         />
+                        <label for="case_sensitive">Case Sensitive</label>
+                    </span>
+                    <span>
+                        <input type="checkbox"
+                               name="wild"
+                               id="wild"
+                               value="wild"
+                               <?=Utils::checkboxState($check['wild'])?>
+                         />
+                        <label for="wild">Wildcard (*)</label>
+                    </span>
+                    <span>
+                        <input type="checkbox"
+                               name="whole_word"
+                               id="whole_word"
+                               value="whole_word"
+                               <?=Utils::checkboxState($check['whole_word'])?>
+                        />
+                        <label for="whole_word">Whole Word</label>
+                    </span>
+                    <span>
+                        <input type="checkbox"
+                               name="perfect_match"
+                               id="perfect_match"
+                               value="perfect_match"
+                               <?=Utils::checkboxState($check['perfect_match'])?>
+                        />
+                        <label for="perfect_match">Perfect Match</label>
+                    </span>
+
+                    <?php if ($check['t2t'] == 't2t') :?>
+                    <input type="hidden"
+                           name="t2t"
+                           id="t2t"
+                           value="t2t"
+                           <?=Utils::checkboxState($check['t2t'], 't2t')?> onclick="uncheckAll();"
                     />
-                    <label for="whole_word">Whole Word</label>
-                </span>
-                <span>
-                    <input type="checkbox"
-                           name="perfect_match"
-                           id="perfect_match"
-                           value="perfect_match"
-                           <?=Utils::checkboxState($check['perfect_match'])?>
-                    />
-                    <label for="perfect_match">Perfect Match</label>
-                </span>
-
-                <?php if ($check['t2t'] == 't2t') :?>
-                <input type="hidden"
-                       name="t2t"
-                       id="t2t"
-                       value="t2t"
-                       <?=Utils::checkboxState($check['t2t'], 't2t')?> onclick="uncheckAll();"
-                />
-                <?php endif; ?>
-            </fieldset>
+                    <?php endif; ?>
+                </fieldset>
+            </div>
+            <p id="displaysearchoptions" class="smallscreen_notices"><a class="toggle-searchoptions-link" href="">⇓ Display search options ⇓</a></p>
         </fieldset>
  </form>
 
