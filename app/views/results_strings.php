7a25d1cc08e590d3818f77a9e7c995b3802e8d44
pascalc <pascal.chevrel@free.fr>
2014-06-23 10:14:45 +0200

    fix html in new filter search buttons, closing div should be before the tables, not after

--- app/views/results_strings.php
+++ app/views/results_strings.php
@@ -1,11 +1,10 @@
 <div id="filters">
     <h4>Filter by folder:</h4>
     <a href="#showall" id="showall" class="filter">Show all results</a>
+    <?=$filter_block;?>
+</div>
+
 <?php
-print $filter_block;
 foreach($output as $results_table) {
     print $results_table;
 }
-
-?>
-</div>
