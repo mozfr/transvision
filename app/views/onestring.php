40b13447316bbba2572020f54904ce048400841b
pascalc <pascal.chevrel@free.fr>
2014-09-03 18:32:59 +0200

    Issue #325: anchors for rows in onestring view

--- app/views/onestring.php
+++ app/views/onestring.php
@@ -1,30 +1,32 @@
 <?php
+use \Transvision\RTLSupport;
 
 // Error management
 if (isset($error)) {
-	if ($error == 1) {
-		print "<p>No entity asked for, goodbye.</p>";
-	} elseif ($error == 2) {
-		print "<p>Entity does not exist for this repo, goodbye.</p>";
-	}
+    if ($error == 1) {
+        print "<p>No entity asked for, goodbye.</p>";
+    } elseif ($error == 2) {
+        print "<p>Entity does not exist for this repo, goodbye.</p>";
+    }
 
-	return;
+    return;
 }
 
 // We have no error, display results
 $page_descr = $entity;
-
 ?>
 
 <table>
-	<tr>
-		<th>Locale</th>
-		<th>Translation</th>
-	</tr>
-	<?php foreach($translations as $k => $v): ?>
     <tr>
-    	<th><?=$k?></th>
-    	<td lang="<?=$k?>"><?=$v?></td>
+        <th>Locale</th>
+        <th>Translation</th>
     </tr>
-	<?php endforeach; ?>
+
+    <?php foreach($translations as $k => $v): ?>
+    <tr id="<?=$k?>">
+        <th><a href="#<?=$k?>"><?=$k?></a></th>
+        <td lang="<?=$k?>" <?=RTLSupport::isRTL($k) ? 'dir="rtl"' : '';?>><?=$v?></td>
+    </tr>
+    <?php endforeach; ?>
+
 </table>
