390b82b8100fd7f5c6bf81c6196d85c5d0c57a0e
pascalc <pascal.chevrel@free.fr>
2014-09-29 17:31:27 +0200

    relnotes

--- app/inc/constants.php
+++ app/inc/constants.php
@@ -1,7 +1,7 @@
 <?php
 
 // Bump this constant with each new release
-const VERSION = '3.5dev';
+const VERSION = '3.5';
 
 // Constants for the project
 define('DATA_ROOT',     $server_config['root']);
