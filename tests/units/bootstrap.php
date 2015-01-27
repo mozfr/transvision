2c10be5e3dfdb36c6b77b7402d89703e81056be4
Pascal Chevrel <pascal.chevrel@free.fr>
2015-01-27 10:06:44 +0100

    PHP 7 compatibility - fix a test about sorting (http://marc.info/?l=php-internals&m=142230002429001&w=2) - add a timezone in bootstrap to avoid a warning

--- tests/units/bootstrap.php
+++ tests/units/bootstrap.php
@@ -5,7 +5,11 @@ define('INSTALL_ROOT',  realpath(__DIR__ . '/../') . '/');
 define('TEST_FILES', realpath(__DIR__ . '/../testfiles/') . '/');
 define('APP_SOURCES', realpath(__DIR__ . '/../testfiles/config') . '/');
 
+// We always work with UTF8 encoding
 mb_internal_encoding('UTF-8');
+
+// Make sure we have a timezone set
+date_default_timezone_set('Europe/Paris');
 
 const DEBUG = true;
 const CACHE_ENABLED = true;
