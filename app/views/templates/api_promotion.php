2b6d5a9581dbf22a2ba2565f1007cba30b052892
Pascal Chevrel <pascal.chevrel@free.fr>
2015-02-04 15:13:47 +0100

    Issue #431: promote API version of search pages * creation of a new Utils::redirectToAPI() function * new views/templates/ folder, template.php is now there and named base.php * Onestring and main search views updated to diplay the API promo

---
+++ app/views/templates/api_promotion.php
@@ -0,0 +1,4 @@
+<p class="api_link">
+    <span>API</span>These results are also available as an <a href="<?=\Transvision\Utils::redirectToAPI()?>">API request</a>.<br>
+    <a href="https://github.com/mozfr/transvision/wiki/JSON-API">Learn more about Transvision API</a>.
+</p>
