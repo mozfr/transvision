6f5c1fdc8d1a90822e7b8e054461ebee943bddf5
Pascal Chevrel <pascal.chevrel@free.fr>
2015-04-09 17:25:50 +0200

    3.7 release prep work

--- app/inc/constants.php
+++ app/inc/constants.php
@@ -1,7 +1,7 @@
 <?php
 
 // Bump this constant with each new release
-const VERSION = '3.7dev';
+const VERSION = '3.7';
 
 // Constants for the project
 define('DATA_ROOT',     $server_config['root']);
