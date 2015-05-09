85ed383c534841cd4551653c62c2c6d2426329ae
TheoChevalier <theo.chevalier11@gmail.com>
2015-05-09 07:35:47 +0200

    Allow developers to toggle PERF_CHECK from config.ini

--- app/inc/constants.php
+++ app/inc/constants.php
@@ -30,5 +30,5 @@ if (file_exists(CACHE_PATH . 'lastdataupdate.txt')) {
 // Special modes for the app
 define('DEBUG', (strstr(VERSION, 'dev') || isset($_GET['debug'])) ? true : false);
 
-// Set to True to log page time generation and memory used while in DEBUG mode
-const PERF_CHECK = false;
+// Set perf_check=true in config.ini to log page time generation and memory used while in DEBUG mode
+define('PERF_CHECK', isset($server_config['perf_check']) ? $server_config['perf_check'] : false);
