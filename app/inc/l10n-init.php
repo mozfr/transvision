mozfr/transvision

commit 3a8874d6092906a74cb28e405ac2f89a7fa3e1bd

Issue #666  Warning in log related to API promotion

app/inc/l10n-init.php
@@ -57,16 +57,6 @@ if (isset($_GET['locale2'])) {
     }
 }
 
-// Bypass default source locale for locale to locale comparison
-if (isset($_GET['sourcelocale'])) {
-    // Redirect locale to a different one if necessary
-    $requested_sourcelocale = $_GET['sourcelocale'];
-    $requested_sourcelocale = Project::getLocaleInContext($requested_sourcelocale, $repo);
-    if (in_array($requested_sourcelocale, $all_locales)) {
-        $source_locale = $requested_sourcelocale;
-    }
-}
-
 // Get RTL attribute for source and target locales
 $locale_dir = $l10n->getDirection($locale);
 $source_locale_dir = $l10n->getDirection($source_locale);

app/classes/Transvision/Utils.php
@@ -461,30 +461,34 @@ class Utils
      * This is used on views which also exist in our public API
      * https://github.com/mozfr/transvision/wiki/JSON-API
      *
-     * @param  boolean $swap_locales Defaults to False. If set to True, swap the
-     *                               values of locale and source_locale parameters.
-     * @return string  URL with 'json' appended as part of the query string
+     * @return string URL with 'json' appended as part of the query string
      */
-    public static function redirectToAPI($swap_locales = false)
+    public static function redirectToAPI()
     {
-        if (! $swap_locales) {
-            return $_SERVER['REQUEST_URI'] . (is_null($_SERVER['QUERY_STRING']) ? '?json=true' : '&json=true');
-        }
+        return $_SERVER["REQUEST_URI"] . (is_null($_SERVER['QUERY_STRING']) ? '?json' : '&json');
+    }
 
+    /**
+     * Return the current URL with the json GET variable appended
+     * and also adds locale codes if they are not already provided
+     * by redirectToAPI()
+     *
+     * @param  string $source_locale Locale code for source
+     * @param  string $target_locale Locale code for target
+     * @return string URL with 'json' appended as part of the query string
+     */
+    public static function APIPromotion($source_locale, $target_locale)
+    {
         // We are going to split and then rebuild QUERY_STRING
-        parse_str($_SERVER['QUERY_STRING'], $args);
+        parse_str(self::redirectToAPI(), $args);
 
-        // We add a json parameter to the query, its value doesn't matter.
-        $args['json'] = 'true';
-
-        // Swap the values of locale and sourcelocale if they exist
-        if (isset($args['locale'], $args['sourcelocale'])) {
-            list($args['locale'], $args['sourcelocale']) = [$args['sourcelocale'], $args['locale']];
-        }
+        $args['locale']       = $source_locale;
+        $args['sourcelocale'] = $target_locale;
+        $args['json']         = 'true';
 
         // We don't want to encode slashes in searches for entity names
         $query = urldecode(http_build_query($args));
 
-        return explode('?', $_SERVER['REQUEST_URI'])[0] . '?' . $query;
+        return $query;
     }
 }

app/views/templates/api_promotion.php
@@ -1,19 +1,20 @@
 <?php
-    if ($controller == 'mainsearch') {
-        ?>
-<p class="api_link">
-    <span>API</span>These results are also available as an API request for <a href="<?=\Transvision\Utils::redirectToAPI()?>"><?=$requested_sourcelocale?></a> or <a href="<?=\Transvision\Utils::redirectToAPI(true)?>"><?=$requested_locale?></a>.<br>
-    <a href="https://github.com/mozfr/transvision/wiki/JSON-API">Learn more about the Transvision API</a>.
-</p>
-<?php
+namespace Transvision;
 
-    } else {
-        ?>
-<p class="api_link">
-    <span>API</span>These results are also available as an <a href="<?=\Transvision\Utils::redirectToAPI()?>">API request</a>.<br>
-    <a href="https://github.com/mozfr/transvision/wiki/JSON-API">Learn more about the Transvision API</a>.
-</p>
-<?php
-
-    }
+if ($controller == 'mainsearch'):
 ?>
+<p class="api_link">
+    <span>API</span>These results are also available as an API request for
+    <a href="<?=Utils::APIPromotion($source_locale, $locale)?>"><?=$source_locale?></a> or
+    <a href="<?=Utils::APIPromotion($locale, $source_locale)?>"><?=$locale?></a>.
+    <br>
+    <a href="https://github.com/mozfr/transvision/wiki/JSON-API">Learn more about the Transvision API</a>.
+</p>
+<?php else: ?>
+
+<p class="api_link">
+    <span>API</span>These results are also available as an <a href="<?=Utils::redirectToAPI()?>">API request</a>.
+    <br>
+    <a href="https://github.com/mozfr/transvision/wiki/JSON-API">Learn more about the Transvision API</a>.
+</p>
+<?php endif; ?>

tests/units/Transvision/Utils.php
@@ -478,18 +478,15 @@ class Utils extends atoum\test
         return [
             [
                 '/string/?entity=browser/chrome/browser/downloads/downloads.properties:stateStarting&repo=central',
-                '/string/?entity=browser/chrome/browser/downloads/downloads.properties:stateStarting&repo=central&json=true',
-                '/string/?entity=browser/chrome/browser/downloads/downloads.properties:stateStarting&repo=central&json=true',
+                '/string/?entity=browser/chrome/browser/downloads/downloads.properties:stateStarting&repo=central&json',
             ],
             [
                 '/v1/versions/',
-                '/v1/versions/?json=true',
-                '/v1/versions/?json=true',
+                '/v1/versions/?json',
             ],
             [
                 '/?recherche=home&repo=aurora&sourcelocale=en-US&locale=fr&search_type=strings',
-                '/?recherche=home&repo=aurora&sourcelocale=en-US&locale=fr&search_type=strings&json=true',
-                '/?recherche=home&repo=aurora&sourcelocale=fr&locale=en-US&search_type=strings&json=true',
+                '/?recherche=home&repo=aurora&sourcelocale=en-US&locale=fr&search_type=strings&json',
             ],
         ];
     }
@@ -497,7 +494,7 @@ class Utils extends atoum\test
     /**
      * @dataProvider redirectToAPIDP
      */
-    public function testRedirectToAPI($a, $b, $c)
+    public function testRedirectToAPI($a, $b)
     {
         $obj = new _Utils();
         $_SERVER['REQUEST_URI'] = $a;
@@ -507,9 +504,29 @@ class Utils extends atoum\test
         $this
             ->string($obj->redirectToAPI())
                 ->isEqualTo($b);
+    }
+    public function APIPromotionDP()
+    {
+        return [
+            [
+                '/?recherche=test&repo=aurora&sourcelocale=fr&locale=en-US&search_type=strings',
+                '/?recherche=test&repo=aurora&sourcelocale=fr&locale=en-US&search_type=strings&json=true',
+            ],
+        ];
+    }
+    /**
+     * @dataProvider APIPromotionDP
+     */
+    public function testAPIPromotion($a, $b)
+    {
+        $obj = new _Utils();
+        $_SERVER['REQUEST_URI'] = $a;
+        $_SERVER['QUERY_STRING'] = isset(parse_url($a)['query'])
+            ? parse_url($a)['query']
+            : null;
         $this
-            ->string($obj->redirectToAPI(true))
-                ->isEqualTo($c);
+            ->string($obj->APIPromotion('en-US', 'fr'))
+                ->isEqualTo($b);
     }
 
     public function testGetScriptPerformances()

commit b1b756efb41dc628f1c271ca5aaffe7ffba4e281

revert a regression I introduced in 3a8874d6092906a74cb28e405ac2f89a7fa3e1bd, I removed this block of code by mistake

app/inc/l10n-init.php
@@ -57,6 +57,16 @@ if (isset($_GET['locale2'])) {
     }
 }
 
+// Bypass default source locale for locale to locale comparison
+if (isset($_GET['sourcelocale'])) {
+    // Redirect locale to a different one if necessary
+    $requested_sourcelocale = $_GET['sourcelocale'];
+    $requested_sourcelocale = Project::getLocaleInContext($requested_sourcelocale, $repo);
+    if (in_array($requested_sourcelocale, $all_locales)) {
+        $source_locale = $requested_sourcelocale;
+    }
+}
+
 // Get RTL attribute for source and target locales
 $locale_dir = $l10n->getDirection($locale);
 $source_locale_dir = $l10n->getDirection($source_locale);