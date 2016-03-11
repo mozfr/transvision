ca48f84646e7d2fecf5dbbe2ae103f5f721ebb1d
Francesco Lodolo (:flod) <flod@lodolo.net>
2016-03-11 16:48:58 +0100

    Fix nits in credits

--- app/views/credits.php
+++ app/views/credits.php
@@ -1,4 +1,4 @@
-<p>Transvision 1.0 was created by Philippe Dessante, former team leader for the French Mozilla localization team handled by the FrenchMozilla non-profit.</p>
+<p>Transvision 1.0 was created by Philippe Dessante, former team leader of the French Mozilla localization team handled by the FrenchMozilla non-profit.</p>
 
 <p>In 2012, FrenchMozilla localization activities were merged with the wider MozFR organization. As part of this process, Transvision maintenance and developments since 1.0 were transferred to MozFR and the application is currently maintained by Pascal Chevrel.</p>
 
@@ -11,23 +11,21 @@
     <li>Théo Chevalier</li>
 </ul>
 
-<p>You can find the full list on our <a href="https://github.com/mozfr/transvision/graphs/contributors">GitHub page</a>. Many localizers and in particular localizers from the French team have participated in Transvision by giving feedback and making feature requests.</p>
+<p>You can find the full list on our <a href="https://github.com/mozfr/transvision/graphs/contributors">GitHub page</a>. Many localizers, and in particular localizers from the French team, have contributed to Transvision by giving feedback and making feature requests.</p>
 
 <p>Transvision was created thanks to the following Open Source Software:</p>
 <ul>
     <li>PHP</li>
     <li>Bash for installation scripts and cron jobs</li>
-    <li>a bit of Python (and specifically, the Silme library written by Zbigniew Braniecki)</li>
+    <li>A bit of Python (and specifically, the Silme library written by Zbigniew Braniecki)</li>
     <li><a href="https://getcomposer.org/">Composer</a>, a dependency manager for PHP projects</li>
     <li><a href="https://www.atoum.org/">Atoum</a>, a unit testing framework for PHP</li>
     <li><a href="https://www.jquery.com/">jQuery</a>, a JavaScript library</li>
     <li><a href="https://wwww.travis-ci.com/">Travis CI</a></li>
 </ul>
 
-<p>Transvision is available on <a href="https://github.com/mozfr/transvision/">GitHub</a></p>
+<p>The Transvision logo was created by Elio Qoshi from the <a href="https://github.com/mozilla/Community-Design">Mozilla Community Design team</a>.</p>
 
-<p>The Transvision logo was created by Elio Qoshi from the <a href="https://github.com/mozilla/Community-Design">Mozilla Community Design team</a></p>
-
-<p>Statistics about the project can be found on <a href="https://www.ohloh.net/p/transvision">Transvision Oloh page</a>.</p>
+<p>Transvision’s code is available on <a href="https://github.com/mozfr/transvision/">GitHub</a>. Statistics about the project can be found on <a href="https://www.ohloh.net/p/transvision">Transvision Oloh page</a>.</p>
 
 <p>Transvision is a community project under the MozFR umbrella. The server is sponsored and hosted by <a href="https://www.gandi.net/">Gandi</a>.</p>
