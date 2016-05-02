85cb8d3fdfc3e3f9e84295b0008d2c48d1d26799
Pascal Chevrel <pascal@mozilla.com>
2016-05-02 15:24:10 +0200

    Fix various issues with the Transliteration API (#742)

--- app/models/api/transliterate.php
+++ app/models/api/transliterate.php
@@ -1,27 +1,24 @@
 <?php
 namespace Transvision;
 
-// Check that the class exists before trying to use it
-// Requires php-intl
+// Check that the class exists before using it. Requires the php-intl extension
 if (! class_exists('Transliterator')) {
     $request->error = 'Class Transliterator not available';
-    $json = $request->invalidAPICall(501);
-
-    return;
-}
-
-switch ($request->parameters[2]) {
-    case 'sr-Cyril':
-        $transliterated_locale = 'Serbian-Latin/BGN';
-        break;
-
-    default:
-    $request->error = 'Wrong language';
 
     return $request->invalidAPICall(501);
 }
 
-$transliterator = \Transliterator::create($transliterated_locale);
-$transliterated_string = $transliterator->transliterate($request->parameters[3]);
+switch ($request->parameters[2]) {
+    case 'sr-Cyrl':
+        $transliterated_locale = 'Serbian-Latin/BGN';
+        break;
 
-return $json = [$transliterator->transliterate(Utils::secureText($request->parameters[3]))];
+    default:
+    $request->error = 'Wrong locale code';
+
+    return $request->invalidAPICall();
+}
+
+$transliterator = \Transliterator::create($transliterated_locale);
+
+return [$transliterator->transliterate(Utils::secureText($request->parameters[3]))];
