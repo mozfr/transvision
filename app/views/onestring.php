9ff28d29e9d22c6e76bb5eb17aa2424d87313cea
Théo Chevalier <theo.chevalier11@gmail.com>
2016-07-05 11:55:30 +0200

    Add warning on all translations view

--- app/views/onestring.php
+++ app/views/onestring.php
@@ -31,9 +31,15 @@ include VIEWS . 'templates/api_promotion.php';
         $rtl_support = RTLSupport::isRTL($locale) ? 'dir="rtl"' : '';
         $search_link = "/?sourcelocale={$reference_locale}&locale={$locale}&repo={$repo}&search_type=entities&recherche={$entity}&perfect_match=perfect_match";
         echo "<tr id='{$locale}'>\n" .
-             "  <th><a href='#{$locale}'>{$locale}</a></th>\n" .
-             "  <td lang='#{$locale}' {$rtl_support} >" . Utils::secureText($translation) . "</td>\n" .
-             "  <td><a class='onestring_search' href='{$search_link}' title='Search for the entity in this locale'>🔍</a></td>\n" .
+             "  <th><a href='#{$locale}'>{$locale}</a></th>\n";
+
+        if (! $translation) {
+            echo "  <td><em class='error'>Warning: Missing string</em></td><td></td>\n";
+        } else {
+            echo "  <td lang='#{$locale}' {$rtl_support} >" . Utils::secureText($translation) . "</td>\n" .
+                 "  <td><a class='onestring_search' href='{$search_link}' title='Search for the entity in this locale'>🔍</a></td>\n";
+        }
+
              "</tr>\n";
     }
     ?>
