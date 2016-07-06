35f9d7222da6342d66e5c29fd598b0b9ffcc274e
Francesco Lodolo (:flod) <flod@lodolo.net>
2016-07-06 11:13:19 +0200

    Fix message in onestring view for empty strings

--- app/views/onestring.php
+++ app/views/onestring.php
@@ -34,7 +34,7 @@ include VIEWS . 'templates/api_promotion.php';
              "  <th><a href='#{$locale}'>{$locale}</a></th>\n";
 
         if (! $translation) {
-            echo "  <td><em class='error'>Warning: Missing string</em></td><td></td>\n";
+            echo "  <td><em class='error'>Warning: Empty string</em></td><td></td>\n";
         } else {
             echo "  <td lang='#{$locale}' {$rtl_support} >" . Utils::secureText($translation) . "</td>\n" .
                  "  <td><a class='onestring_search' href='{$search_link}' title='Search for the entity in this locale'>🔍</a></td>\n";
