65cae284651fd465a9461532f805b432e2dc6aeb
Théo Chevalier <theo.chevalier11@gmail.com>
2016-08-03 11:24:29 +0200

    Update release notes layout for multiline items (#789)

--- app/views/changelog.php
+++ app/views/changelog.php
@@ -20,11 +20,12 @@ foreach ($changelog as $release => $changes) {
 
         $output .= "  <li>";
         $output .= isset($attributes['type']) ? $relnotes($attributes['type'][0]) . ' ' : '';
+        $output .= '<div>';
         $output .= isset($attributes['issues']) ? $issue($attributes['issues']) : '';
         $output .= isset($attributes['commit']) ? $commit($attributes['commit']) : '';
         $output .= $attributes['message'][0];
         $output .= isset($attributes['authors']) ? $authors($attributes['authors']) : '';
-        $output .= "</li>\n";
+        $output .= "</div></li>\n";
     }
     $output .= "</ul>\n";
     $output .= $github_link($release);
