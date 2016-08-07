0a35dc2489106522aa793479f65449a098613650
Théo Chevalier <theo.chevalier11@gmail.com>
2016-08-07 13:12:00 +0200

    Fixes #802 - @@missing@@ shown instead of "Missing string" for string searches

--- app/classes/Transvision/ShowResults.php
+++ app/classes/Transvision/ShowResults.php
@@ -312,6 +312,9 @@ class ShowResults
                 $target_string2 = '';
             }
 
+            $target_string_missing  = $target_string  == '@@missing@@';
+            $target_string2_missing = $target_string2 == '@@missing@@';
+
             $string_id = md5($key . mt_rand());
             $regular_string_id = 'string_' . $string_id;
 
@@ -320,7 +323,7 @@ class ShowResults
                 The string gets transliterated if the target local is Serbian,
                 if we aren't in the 3locales view and if we have a $target_string
             */
-            $transliterate = $locale2 == 'sr' && ! $extra_locale && $target_string && $target_string != '@@missing@@';
+            $transliterate = $locale2 == 'sr' && ! $extra_locale && $target_string && ! $target_string_missing;
 
             if ($transliterate) {
                 $transliterated_string = self::getTransliteratedString(urlencode($target_string), 'sr-Cyrl');
@@ -398,7 +401,7 @@ class ShowResults
             }
 
             // If there is no target_string, display an error, otherwise display the string + meta links
-            if ($target_string == '@@missing@@') {
+            if ($target_string_missing) {
                 $target_string = '<em class="error">Warning: Missing string</em>';
             } elseif (! $target_string) {
                 $target_string = '<em class="error">Warning: Empty string</em>';
@@ -411,7 +414,7 @@ class ShowResults
             }
 
             // If there is no target_string2, display an error, otherwise display the string + meta links
-            if ($target_string2 == '@@missing@@') {
+            if ($target_string2_missing) {
                 $target_string2 = '<em class="error">Warning: Missing string</em>';
             } elseif (! $target_string2) {
                 $target_string2 = '<em class="error">Warning: Empty string</em>';
