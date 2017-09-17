a6ed21f23b4b573d5e4037c88b9734ad47931319
Francesco Lodolo <flod@lodolo.net>
2017-09-17 18:44:11 +0200

    (fixes #855) Display warning when search has trailing or leadingwhitespaces (#887)

--- app/views/results_strings.php
+++ app/views/results_strings.php
@@ -9,6 +9,14 @@
 <?php
 endif;
 
+if ($search->getSearchTerms() != trim($search->getSearchTerms())) {
+    parse_str($_SERVER['QUERY_STRING'], $args);
+    $args['recherche'] = trim($search->getSearchTerms());
+    $query = urldecode(http_build_query($args));
+    echo '<p id="search_warning"><strong>Warning:</strong> the current search includes leading or trailing whitespaces.<br/> <a href="/?'
+        . $query . '">Click here</a> to perform the same search without whitespaces.</p>';
+}
+
 foreach ($output as $results_table) {
     print $results_table;
 }
