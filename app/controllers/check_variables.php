12d06a8a67f4577429edc781580ce7f5a2494635
Francesco Lodolo <flod@lodolo.net>
2017-11-27 12:55:44 +0100

    Add json parameter to variable and shortcuts views (#923)

--- app/controllers/check_variables.php
+++ app/controllers/check_variables.php
@@ -5,4 +5,14 @@ namespace Transvision;
 require_once INC . 'l10n-init.php';
 
 include MODELS . 'check_variables.php';
+
+/*
+    Used only for QA reasons: adding &json to the query will shortcut the logic,
+    and display the results as JSON instead of loading view and template.
+*/
+if (isset($_GET['json'])) {
+    $json = $var_errors;
+    include VIEWS . 'json.php';
+    die();
+}
 include VIEWS . 'check_variables.php';
--- app/controllers/commandkeys.php
+++ app/controllers/commandkeys.php
@@ -5,4 +5,14 @@ namespace Transvision;
 require_once INC . 'l10n-init.php';
 
 include MODELS . 'commandkeys.php';
+
+/*
+    Used only for QA reasons: adding &json to the query will shortcut the logic,
+    and display the results as JSON instead of loading view and template.
+*/
+if (isset($_GET['json'])) {
+    $json = $commandkey_results;
+    include VIEWS . 'json.php';
+    die();
+}
 include VIEWS . 'commandkeys.php';
