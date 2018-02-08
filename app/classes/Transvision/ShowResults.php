f53a8a70dfb6f171b2e92744740301bf8b158938
Francesco Lodolo [:flod] <flodolo@mozilla.com>
2018-02-08 15:15:07 +0100

    Fix issue #935, wrong edit link for other-licenses

--- app/classes/Transvision/ShowResults.php
+++ app/classes/Transvision/ShowResults.php
@@ -306,6 +306,7 @@ class ShowResults
                 case 'chat':
                 case 'editor':
                 case 'mail':
+                case 'other-licenses':
                     $project_name = 'thunderbird';
                     break;
                 case 'mobile':
