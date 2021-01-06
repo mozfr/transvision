38ad0ec359c22dc58c0dd03a464ffaa49f66181a
Francesco Lodolo [:flod] <flodolo@mozilla.com>
2021-01-06 18:27:41 +0100

    Reformat files with black

--- app/scripts/ini_to_bash.py
+++ app/scripts/ini_to_bash.py
@@ -7,12 +7,11 @@ from configparser import ConfigParser
 def main():
     # Parse command line options
     cl_parser = argparse.ArgumentParser()
-    cl_parser.add_argument(
-        'config_path', help='Path to INI configuration file')
+    cl_parser.add_argument("config_path", help="Path to INI configuration file")
     args = cl_parser.parse_args()
 
     parser = ConfigParser()
-    with open(args.config_path, 'r') as f:
+    with open(args.config_path, "r") as f:
         parser.read_file(f)
 
     for sec in parser.sections():
@@ -20,5 +19,5 @@ def main():
             print('{}="{}"'.format(key, val))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
