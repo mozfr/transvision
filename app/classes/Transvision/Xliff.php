6b488037cd24c5d3ed59540ffc459de9d863a1cb
Francesco Lodolo <flod@lodolo.net>
2025-04-18 16:17:51 +0200

    Use original IDs if possible when extracting XLIFF strings (#1047)

--- app/classes/Transvision/Xliff.php
+++ app/classes/Transvision/Xliff.php
@@ -38,7 +38,7 @@ class Xliff
                 $file_node = $trans_unit->xpath('../..');
                 $file_orig = $file_node[0]['original'];
 
-                $string_id = self::generateStringID($project_name, $file_name, $file_orig, $trans_unit['id']);
+                $string_id = self::generateStringID($project_name, $file_name, $file_orig, $trans_unit['id'], $strings);
 
                 if ($reference_locale) {
                     // If it's the reference locale, we use the source instead of the target
@@ -60,18 +60,36 @@ class Xliff
 
     /**
      * Generate a unique ID for a string to store in Transvision.
-     * String ID can be identical to the source string in iOS, so it's more
-     * reliable to generate a unique ID from it.
+     *
+     * If the string ID includes a space, assume it's text, and generate a
+     * unique identifier based on the file and ID. If there is already a string
+     * with that ID extracted, add the hash.
+     *
      *
      * @param string $project_name The project this string belongs to
      * @param string $file_name    .xliff file name
      * @param string $file_orig    'original' attribute of the element's parent
      * @param string $string_id    'id' attribute of the <trans-unit> element
+     * @param string $strings      strings already extracted from the file
      *
-     * @return string unique ID such as firefox_ios/firefox-ios.xliff:1dafea7725862ca854c408f0e2df9c88
+     * @return string Either string ID, or unique ID based on the file and ID
+     *                firefox_ios/firefox-ios.xliff:1dafea7725862ca854c408f0e2df9c88
      */
-    public static function generateStringID($project_name, $file_name, $file_orig, $string_id)
+    public static function generateStringID($project_name, $file_name, $file_orig, $string_id, $strings)
     {
-        return "{$project_name}/{$file_name}:" . hash('md5', $file_orig . $string_id);
+        // If $string_id contains a space, generate unique ID.
+        if (strpos($string_id, ' ') !== false) {
+            return "{$project_name}/{$file_name}:" . hash('md5', $file_orig . $string_id);
+        }
+
+        $generated_id = "{$project_name}/{$file_name}:{$string_id}";
+
+        // If we already extracted a string with the same ID, use a more unique ID.
+        if (array_key_exists($generated_id, $strings)) {
+            // If already defined, fall back to the generated ID.
+            return "{$generated_id}-" . hash('md5', $file_orig . $string_id);
+        }
+
+        return $generated_id;
     }
 }
